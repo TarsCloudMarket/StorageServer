a60191ed323a186d4fbabc6924fa528ad91ed458
ruanshudong <ruanshudong@qq.com>
2022-02-18 17:31:31 +0800

    fix initialize use storage.conf

--- lib/StorageServer.cpp
+++ lib/StorageServer.cpp
@@ -10,39 +10,43 @@
 
 void StorageServer::initialize()
 {
-	string dataPath = ServerConfig::DataPath;
+	string dataPath;
+	TC_Config conf;
 
-	if(!TC_Port::getEnv("KUBERNETES_PORT").empty())
-	{
-		dataPath = "/storage-data/";
-	}
+	LOG_CONSOLE_DEBUG << "data path:" << ServerConfig::DataPath << endl;
 
 	//for debug
 	if(ServerConfig::DataPath == "./debug-data/")
 	{
+		dataPath = ServerConfig::DataPath;
+
 		_index = TC_Endpoint(getConfig().get("/tars/application/server/Base.StorageServer.RaftObjAdapter<endpoint>", "")).getPort();
 
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10101"), TC_Endpoint("tcp -h 127.0.0.1 -p 10401 -t 60000")));
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10102"), TC_Endpoint("tcp -h 127.0.0.1 -p 10402 -t 60000")));
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10103"), TC_Endpoint("tcp -h 127.0.0.1 -p 10403 -t 60000")));
 	}
+	else
+	{
+		addConfig("storage.conf");
+		conf.parseFile(ServerConfig::BasePath + "storage.conf");
+
+		dataPath = conf.get("/root<storage-path>");
+	}
 
 	LOG_CONSOLE_DEBUG << "data path:" << ServerConfig::DataPath << ", index:" << _index << ", node size:" << _nodeInfo.nodes.size() << endl;
 
-	addConfig("storage.conf");
-	TC_Config conf;
-	conf.parseFile(ServerConfig::BasePath + "storage.conf");
 
 	RaftOptions raftOptions;
 	raftOptions.electionTimeoutMilliseconds = TC_Common::strto<int>(conf.get("/root/raft<electionTimeoutMilliseconds>", "3000"));
 	raftOptions.heartbeatPeriodMilliseconds = TC_Common::strto<int>(conf.get("/root/raft<electionTimeoutMilliseconds>", "300"));
 	raftOptions.snapshotPeriodSeconds       = TC_Common::strto<int>(conf.get("/root/raft<snapshotPeriodSeconds>", "600"));
-	raftOptions.maxLogEntriesPerRequest     = TC_Common::strto<int>(conf.get("/root/raft<snapshotPeriodSeconds>", "100"));
-	raftOptions.maxLogEntriesMemQueue       = TC_Common::strto<int>(conf.get("/root/raft<snapshotPeriodSeconds>", "3000"));
-	raftOptions.maxLogEntriesTransfering    = TC_Common::strto<int>(conf.get("/root/raft<snapshotPeriodSeconds>", "1000"));
-	raftOptions.dataDir                     = dataPath + "raft-log-" + TC_Common::tostr(_index);
+	raftOptions.maxLogEntriesPerRequest     = TC_Common::strto<int>(conf.get("/root/raft<maxLogEntriesPerRequest>", "100"));
+	raftOptions.maxLogEntriesMemQueue       = TC_Common::strto<int>(conf.get("/root/raft<maxLogEntriesMemQueue>", "3000"));
+	raftOptions.maxLogEntriesTransfering    = TC_Common::strto<int>(conf.get("/root/raft<maxLogEntriesTransfering>", "1000"));
+	raftOptions.dataDir                     = TC_File::simplifyDirectory(dataPath + FILE_SEP + "raft-log-" + TC_Common::tostr(_index));
 
-	onInitializeRaft(raftOptions, "StorageObj", dataPath + "StorageLog-" + TC_Common::tostr(_index));
+	onInitializeRaft(raftOptions, "StorageObj", TC_File::simplifyDirectory(dataPath + FILE_SEP + "StorageLog-" + TC_Common::tostr(_index)));
 }
 
 void StorageServer::destroyApp()
--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,6 @@ rm -rf ${PROJECT_BIN_DIR}/StorageServer/1/data
 rm -rf ${PROJECT_BIN_DIR}/StorageServer/2/data
 rm -rf ${PROJECT_BIN_DIR}/StorageServer/3/data
 
-nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../src/count-1.conf > StorageServer_1.log 2>&1 &
-nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../src/count-2.conf > StorageServer_2.log 2>&1 &
-nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../src/count-3.conf > StorageServer_3.log 2>&1 &
+nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../config-1.conf > StorageServer_1.log 2>&1 &
+nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../config-2.conf > StorageServer_2.log 2>&1 &
+nohup ${PROJECT_BIN_DIR}/bin/StorageServer --config=${PROJECT_BIN_DIR}/../config-3.conf > StorageServer_3.log 2>&1 &
