e8a32fa000878186d5c3e1a6b112464c9dae43a6
ruanshudong <ruanshudong@qq.com>
2022-02-22 21:54:55 +0800

    for test

--- lib/StorageServer.cpp
+++ lib/StorageServer.cpp
@@ -22,8 +22,11 @@ void StorageServer::initialize()
 
 		_index = TC_Endpoint(getConfig().get("/tars/application/server/Base.StorageServer.RaftObjAdapter<endpoint>", "")).getPort();
 
+		//_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 172.30.0.3 -p 10101"), TC_Endpoint("tcp -h 172.30.0.3 -p 10401 -t 60000")));
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10101"), TC_Endpoint("tcp -h 127.0.0.1 -p 10401 -t 60000")));
+//		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 172.30.0.33 -p 10102"), TC_Endpoint("tcp -h 172.30.0.33 -p 10402 -t 60000")));
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10102"), TC_Endpoint("tcp -h 127.0.0.1 -p 10402 -t 60000")));
+//		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 172.30.0.34 -p 10103"), TC_Endpoint("tcp -h 172.30.0.34 -p 10403 -t 60000")));
 		_nodeInfo.nodes.push_back(std::make_pair(TC_Endpoint("tcp -h 127.0.0.1 -p 10103"), TC_Endpoint("tcp -h 127.0.0.1 -p 10403 -t 60000")));
 	}
 	else
